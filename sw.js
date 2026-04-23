dcd408e70a7bd6518de866f28f48b7f3bcf1ee33
caitlyncrespo1 <107370476+caitlyncrespo1@users.noreply.github.com>
2026-04-23 15:10:54 -0500

    Delete sw.js

--- sw.js
+++
@@ -1,7 +0,0 @@
-const CACHE = 'freelance-v2';
-self.addEventListener('install', e => e.waitUntil(caches.open(CACHE).then(c => c.addAll(['/index.html'])).then(() => self.skipWaiting())));
-self.addEventListener('activate', e => e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
-self.addEventListener('fetch', e => {
-  if (e.request.url.includes('supabase.co')) return;
-  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
-});
